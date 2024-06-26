3fd7ec4668290f5463ea5c70df5ce54146155e50
obinna <oigbokwe73@gmail.com>
2024-06-26 15:37:16 -0400

    Add project files.

---
+++ roadmaptonigeria/search.cs
@@ -0,0 +1,56 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using Xenhey.BPM.Core.Net8.Implementation;
+using Xenhey.BPM.Core.Net8;
+using Microsoft.Azure.Functions.Worker;
+
+namespace roadmaptonigeria
+{
+    public class registration
+    {
+        private readonly ILogger _logger;
+
+        public registration(ILogger<registration> logger)
+        {
+            _logger = logger;
+        }
+
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [Function("notifications-preferences")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req)
+        {
+            _req = req;
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(_req.Body).ReadToEndAsync();
+            _req.Headers.ToList().ForEach(item => { nvc.Add(item.Key, item.Value.FirstOrDefault()); });
+            var results = orchrestatorService.Run(requestBody);
+            return resultSet(results);
+
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = nvc.Get("Content-Type");
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype;
+            return returnContent;
+        }
+        private IOrchestrationService orchrestatorService
+        {
+            get
+            {
+                return new RemoteOrchrestratorService(nvc);
+            }
+        }
+
+    }
+}
---
+++ roadmaptonigeria/uploadfile.cs
@@ -0,0 +1,56 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using Xenhey.BPM.Core.Net8.Implementation;
+using Xenhey.BPM.Core.Net8;
+using Microsoft.Azure.Functions.Worker;
+
+namespace roadmaptonigeria
+{
+    public class uploadfile
+    {
+        private readonly ILogger _logger;
+
+        public uploadfile(ILogger<uploadfile> logger)
+        {
+            _logger = logger;
+        }
+
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [Function("uploadfile")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req)
+        {
+            _req = req;
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = await new StreamReader(_req.Body).ReadToEndAsync();
+            _req.Headers.ToList().ForEach(item => { nvc.Add(item.Key, item.Value.FirstOrDefault()); });
+            var results = orchrestatorService.Run(requestBody);
+            return resultSet(results);
+
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = nvc.Get("Content-Type");
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype;
+            return returnContent;
+        }
+        private IOrchestrationService orchrestatorService
+        {
+            get
+            {
+                return new ManagedOrchestratorService(nvc);
+            }
+        }
+
+    }
+}}
