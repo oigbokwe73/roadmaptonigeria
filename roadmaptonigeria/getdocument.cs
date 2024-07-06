3956dd6f340be6edd2b7a140e2f390b6e3b5b69a
obinna <oigbokwe73@gmail.com>
2024-07-06 12:41:57 -0400

    update to include file retrival

---
+++ roadmaptonigeria/getdocument.cs
@@ -0,0 +1,57 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Specialized;
+using System.Linq;
+using System.IO;
+using Xenhey.BPM.Core.Net8.Implementation;
+using Xenhey.BPM.Core.Net8;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace roadmaptonigeria
+{
+    public class getdocument
+    {
+        private readonly ILogger _logger;
+
+        public getdocument(ILogger<document> logger)
+        {
+            _logger = logger;
+        }
+
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [Function("getdocument")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous,"get", Route = "fileid")]
+            HttpRequest req, string fileid)
+        {
+            var input = JsonConvert.SerializeObject(new { fileid });
+            _req = req;
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = input;
+            _req.Headers.ToList().ForEach(item => { nvc.Add(item.Key, item.Value.FirstOrDefault()); });
+            var results = orchrestatorService.ReturnFile(requestBody);
+            return resultSet(results);
+
+        }
+
+        private ActionResult resultSet(byte[] reponsePayload)
+        {
+            var mediaSelectedtype = nvc.Get("Content-Type");
+            var returnContent = new FileContentResult(reponsePayload, mediaSelectedtype);
+            return returnContent;
+        }
+        private IOrchestrationService orchrestatorService
+        {
+            get
+            {
+                return new RemoteOrchrestratorService(nvc);
+            }
+        }
+
+    }
+}
